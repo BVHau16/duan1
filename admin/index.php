fa11923147d16bb533a5a092b9275b93a5cbbf74
BVHau16 <haubvph48903@gmail.com>
2025-03-19 08:44:25 +0700

    cập nhật code

--- admin/index.php
+++ admin/index.php
@@ -2,15 +2,15 @@
 session_start();
 ob_start();
 
-if ($_SESSION['user']['loai_nguoi_dung'] === 'NhanVien') {
+// if ($_SESSION['user']['loai_nguoi_dung'] === 'NhanVien') {
   
-} else {
-    // Xóa session nếu không phải nhân viên
-    unset($_SESSION['user']);
-    $_SESSION['thongbao'] = 'Bạn không có quyền truy cập!';
-    header("Location:../index.php?act=dangnhap"); // Quay lại trang đăng nhập
-    exit();
-}
+// } else {
+//     // Xóa session nếu không phải nhân viên
+//     unset($_SESSION['user']);
+//     $_SESSION['thongbao'] = 'Bạn không có quyền truy cập!';
+//     header("Location:../index.php?act=dangnhap"); // Quay lại trang đăng nhập
+//     exit();
+// }
 
 include "../models/pdo.php";
 include "../admin/views/layouts/header.php";
