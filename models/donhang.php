f46fce495a1ff88b0eef82fc611f76e50dda09b8
tatrunghieu9105 <tahieu9105@gmail.com>
2025-04-25 08:29:59 +0700

    cập nhật code

--- models/donhang.php
+++ models/donhang.php
@@ -73,7 +73,7 @@ function update_trang_thai_donhang($id, $trang_thai) {
         return "Chỉ có thể chuyển trạng thái từ 'Chờ xử lý' sang 'Đang giao' hoặc 'Hủy'.";
     }
 
-    if ($current_status === 'Đang giao' && !in_array($trang_thai, ['Đang giao', 'Hủy'])) {
+    if ($current_status === 'Đang giao' && !in_array($trang_thai, ['Hoàn thành', 'Hủy'])) {
         return "Chỉ có thể chuyển trạng thái từ 'Đang giao' sang 'Hoàn thành' hoặc 'Hủy'.";
     }
 
